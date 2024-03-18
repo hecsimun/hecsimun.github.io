8d669f3d22789851147755d0b97628d7a863cb06
hecsimun <71760198+hecsimun@users.noreply.github.com>
2024-03-18 19:13:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function menuOnClick() {
+  document.getElementById("menu-bar").classList.toggle("change");
+  document.getElementById("nav").classList.toggle("change");
+  document.getElementById("menu-bg").classList.toggle("change-bg");
+}
+
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+  document.getElementById("tekstloader").style.display = "none";
+}
